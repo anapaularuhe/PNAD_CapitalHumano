12e6ca91b7e3e202119828ecfa6837441f6112ba
Ana Paula Ruhe <68302747+anapaularuhe@users.noreply.github.com>
2021-10-01 16:58:40 -0300

    Add deflatores

--- PNAD no R.R
+++ PNAD no R.R
@@ -1,19 +1,29 @@
 # PNADC NO R ------------------------------------
 # FGV IBRE - Instituto Brasileiro de Economia
-# NÃºcleo de Mercado de Trabalho
+# Núcleo de Mercado de Trabalho
 # Ana Paula Nothen Ruhe
 
-# DocumentaÃ§Ã£o CRAN: https://cran.r-project.org/web/packages/PNADcIBGE/PNADcIBGE.pdf
+# Documentação CRAN: https://cran.r-project.org/web/packages/PNADcIBGE/PNADcIBGE.pdf
 # Tutorial: https://rpubs.com/gabriel-assuncao-ibge/pnadc
 
-# > PREPARAÃ‡ÃƒO ----------------------------------
-setwd("C:/Users/ana.ruhe/Documents/R")           # Definindo o diretÃ³rio 
+# > PREPARAÇÃO ----------------------------------
+setwd("C:/Users/ana.ruhe/Documents/R")           # Definindo o diretório 
+rm(list = ls())                                  # Equivalente do R a clear all (limpar variáveis amarzenadas na memória)
+
 
 # Pacotes
-  library(PNADcIBGE)                             # NecessÃ¡rio para importar os dados da PNAD  
-  library(survey)                                # NecessÃ¡rio para lidar com pesquisas amostrais
-  library(tidyverse)                             # Pacote de manipulaÃ§Ã£o de dados
+  library(PNADcIBGE)                             # Necessário para importar os dados da PNAD  
+  library(survey)                                # Necessário para lidar com pesquisas amostrais
+  library(dplyr)                                 # Pacote de manipulação de dados
+  library(haven)                                 # Pacote que permite lidar com formato .dta
+ 
+
 
 # Importando dados:
-  pnad2021.2 = get_pnadc(2021, quarter = 2)      # Microdados 2Âº trimestre de 2021; formato "survey"
-  pnad2021.2$variables                           # Data frame
+  pnad2021.2 = get_pnadc(2021, quarter = 2)      # Microdados 2º trimestre de 2021; formato "survey"
+  pnad2021.2.df = pnad2021.2$variables           # Data frame
+
+  
+# Deflatores associados aos microdados:  
+  deflator2021.2 = data.frame("Ano" = pnad2021.2$variables$Ano, "Trimestre" = pnad2021.2$variables$Trimestre, "UF" = pnad2021.2$variables$UF, "Efetivo" = pnad2021.2$variables$Efetivo, "Habitual" = pnad2021.2$variables$Habitual)
+  
